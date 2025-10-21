4420a27d329d7aa3896c5e1c39969d54d1ae7126
Bruno Corrêa <bruno.correa@unico.io>
2025-10-21 15:16:08 -0300

    chore: initial config

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version: 5.5
+import PackageDescription
+
+let package = Package(
+  name: "centralizer-sdk-ios",
+  platforms: [
+    .iOS(.v15)
+  ],
+  products: [
+    .library(
+      name: "centralizer-sdk-ios",
+      targets: ["centralizer-sdk-ios"]
+    ),
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/google/promises",
+      .upToNextMajor(from: "2.3.1")
+    ),
+    .package(
+        url: "https://github.com/acesso-io/unico-check-ios.git",
+        from: "2.16.0"
+    )
+  ],
+  targets: [
+    .target(
+      name: "centralizer-sdk-ios",
+      dependencies: [
+        .product(name: "Promises", package: "promises"),
+        .product(name: "unicocheck-ios-spm", package: "unicocheck-ios-spm")
+      ],
+      resources: [
+        .process("Resources")
+      ],
+      swiftSettings: [
+        .define("INTERNAL")
+      ]
+    ),
+    .testTarget(
+      name: "centralizer-sdk-iosTests",
+      dependencies: ["centralizer-sdk-ios"]
+    ),
+  ]
+)
